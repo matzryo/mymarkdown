045719ad4195a71641271676b808d6a9d7817e68
matzryo <matzryo@gmail.com>
2018-07-21 21:41:27 +0900

    [add] 認証

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <h1>{{ msg }}</h1>
-    <button>Google アカウントでログイン </button>
+    <button @click="googleLogin">Google アカウントでログイン </button>
   </div>
 </template>
 <script>
@@ -11,6 +11,11 @@ export default {
     return {
       msg: "Welcome to MatzMarkdown"
     };
+  },
+  methods: {
+    googleLogin: function () {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
   }
 };
 </script>
